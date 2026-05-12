9550d158b44ff25e8256f9d0fac8f48febf714c3
franly2 <franlybudipramana588@gmail.com>
2026-05-12 16:58:57 +0700

    regist;login;addAccount

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from './../../environments/environment';
-
+// d
 @Injectable({
   providedIn: 'root',
 })
